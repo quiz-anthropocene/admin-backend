089ef1d0c7d663033c9bd5210c53a3dc5a483f2c
Raphael Odini <raphael.odini@protonmail.com>
2020-06-28 11:33:56 +0200

    Frontend: add quiz list page in prerendering list

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -13,7 +13,11 @@ module.exports = {
           // Required - The path to the webpack-outputted app to prerender.
           staticDir: path.join(__dirname, 'dist'),
           // Required - Routes to render.
-          routes: [ '/', '/a-propos', '/contribuer', '/glossaire'],
+          routes: [
+            '/',
+            '/a-propos', '/contribuer', '/glossaire',
+            '/quiz'
+          ],
           // Optional - default None
           // renderAfterTime: 5000, // 5 seconds
           // renderAfterDocumentEvent: 'custom-render-trigger'
