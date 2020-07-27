47fe62b1e9f78c7e6acbb68f6c32a604d56f1417
Raphael Odini <raphael.odini@protonmail.com>
2020-07-27 16:32:44 +0200

    Frontend: fix scroll not going back to top on page change

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -114,6 +114,13 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   routes,
+  // eslint-disable-next-line
+  scrollBehavior (to, from, savedPosition) {
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    return { x: 0, y: 0 };
+  },
 });
 
 export default router;
