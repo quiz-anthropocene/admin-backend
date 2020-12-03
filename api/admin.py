8efa7883b98553ad2e4798b656e74103d227a5f4
Raphael Odini <raphael.odini@protonmail.com>
2020-12-03 15:56:56 +0100

    Backend: create new tags automatically during Notion import

--- api/admin.py
+++ api/admin.py
@@ -338,7 +338,7 @@ class QuestionAdmin(ImportMixin, ExportMixin, admin.ModelAdmin):
             scope = request.POST.get("run_import_questions_from_notion_script")
             management.call_command("import_questions_from_notion", scope, stdout=out)
             notion_questions_validation = out.getvalue()
-            notion_questions_validation = notion_questions_validation.split("|||")
+            notion_questions_validation = notion_questions_validation.split("\n")
             notion_questions_validation = [
                 elem.split("///") if ("///" in elem) else elem
                 for elem in notion_questions_validation
