1f01c4fa071f9aff10d9bc4560fb3a8f71e1dde2
Raphael Odini <raphael.odini@protonmail.com>
2020-12-14 00:31:47 +0100

    DevOps: fix scalingo release task

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,3 +1,2 @@
 python manage.py migrate
-# python manage.py loaddata data/questions.yaml
-python manage.py loaddata data/ressources-glossaire.yaml
+# python manage.py loaddata data/ressources-glossaire.yaml --model=glossary --format=yaml-pretty-flat
