7844c5e044358c9fb17b55b76888f22a58af074a
Raphael Odini <raphael.odini@protonmail.com>
2020-12-25 23:35:50 +0100

    DevOps: move procfile to backend folder

---
+++ backend/release-tasks.sh
@@ -0,0 +1,2 @@
+python manage.py migrate
+# python manage.py loaddata data/ressources-glossaire.yaml --model=glossary --format=yaml-pretty-flat  # won't work if DEPLOY_FOLDER set
--- release-tasks.sh
+++
@@ -1,2 +0,0 @@
-python manage.py migrate
-# python manage.py loaddata data/ressources-glossaire.yaml --model=glossary --format=yaml-pretty-flat
