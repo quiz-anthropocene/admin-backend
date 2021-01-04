bb4cc9f24824f28060407e7026de3aaa522caa00
Raphael Odini <raphael.odini@protonmail.com>
2021-01-04 19:05:14 +0100

    Frontend: try prerendering latest quiz

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -19,6 +19,7 @@ module.exports = {
           routes: [
             '/',
             '/a-propos', '/contribuer', '/glossaire',
+            '/quiz/57'
             // '/quiz',
           ],
           postProcess(renderedRoute) {
