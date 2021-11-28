b2963906b92c2d01d5e158b06d7f5b16dbf58b91
Raphael Odini <raphodn@users.noreply.github.com>
2021-11-28 23:17:01 +0100

    Data: Utiliser l'API de Notion (#1259)
    * Simple authenticated request to Question table with Notion API
    
    * Update import script with new data
    
    * Update views. Quick message in admin

--- api/utilities_notion.py
+++ api/utilities_notion.py
@@ -1,10 +1,94 @@
-from datetime import datetime  # , date
+import requests
+import json
+from datetime import datetime, date
 
 import notion  # https://github.com/jamalex/notion-py
 from notion.client import NotionClient
 
 from django.conf import settings
+from django.db.models.fields import URLField
 
+from api.models import Question
+
+
+TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S"  # "2021-05-05T13:02:00.000Z"  # milliseconds not managed
+QUESTION_URL_FIELDS = [field.name for field in Question._meta.fields if type(field) == URLField]
+
+
+# Official API
+
+def get_question_table_pages(start_cursor=None):
+    """
+    Retrive 100 pages from the question table
+    """
+    url = f"https://api.notion.com/v1/databases/{settings.NOTION_QUESTION_TABLE_ID}/query"
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {settings.NOTION_API_SECRET}", "Notion-Version": f"{settings.NOTION_API_VERSION}"}
+    data = {"sorts": [{"property": "Last edited time", "direction": "descending"}]}
+    if start_cursor:
+        data["start_cursor"] = start_cursor
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    if response.status_code != 200:
+        raise Exception(json.loads(response._content))
+
+    return response
+
+
+def process_page_properties(page_properties):
+    """
+    Fetch & cleanup fields
+    """
+    page_dict = dict()
+    for key in page_properties:
+        # Step 1: process key by Notion 'type"
+        if page_properties[key]["type"] == "title":
+            page_dict[key] = "".join([text["plain_text"] for text in page_properties[key]["title"]])
+        elif page_properties[key]["type"] == "rich_text":
+            page_dict[key] = "".join([text["plain_text"] for text in page_properties[key]["rich_text"]])
+        elif page_properties[key]["type"] == "number":
+            page_dict[key] = page_properties[key]["number"]
+        elif page_properties[key]["type"] == "select":
+            try:
+                page_dict[key] = page_properties[key]["select"]["name"]
+            except (TypeError, AttributeError) as e:
+                page_dict[key] = ""
+        elif page_properties[key]["type"] == "multi_select":
+            page_dict[key] = [tag["name"] for tag in page_properties[key]["multi_select"]]
+        elif page_properties[key]["type"] == "url":
+            try:
+                page_dict[key] = page_properties[key]["url"].strip()
+            except (TypeError, AttributeError) as e:
+                page_dict[key] = ""
+        elif page_properties[key]["type"] == "checkbox":
+            page_dict[key] = page_properties[key]["checkbox"]  # True or False
+        elif page_properties[key]["type"] == "date":
+            try:
+                page_dict[key] = page_properties[key]["date"]["start"]
+            except (TypeError, AttributeError) as e:
+                page_dict[key] = ""
+        elif page_properties[key]["type"] == "created_time":
+            page_dict[key] = page_properties[key]["created_time"]
+        elif page_properties[key]["type"] == "created_by":
+            page_dict[key] = page_properties[key]["created_by"]
+        elif page_properties[key]["type"] == "last_edited_by":
+            page_dict[key] = page_properties[key]["last_edited_by"]
+        elif page_properties[key]["type"] == "last_edited_time":
+            page_dict[key] = page_properties[key]["last_edited_time"]
+        else:
+            raise Exception(f"process_page_properties: {page_properties[key]['type']} missing")
+        
+        # Step 2: process key by custom rules
+        if key == "Text":
+            page_dict["text"] = page_dict[key]
+        if key == "difficulty":
+            page_dict[key] = int(page_dict[key]) if page_dict[key] else None
+        elif key == "added":
+            page_dict[key] = date.fromisoformat(page_dict[key]) if page_dict[key] else datetime.strptime(page_dict["Created time"][:-5], TIMESTAMP_FORMAT).date()
+
+    return page_dict
+
+
+# Unofficial API (notion-py)
 
 def get_notion_client():
     """
