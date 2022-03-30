4a5b634d7f57887fe08b1c84c867024ae0a3cb62
Raphael Odini <raphodn@users.noreply.github.com>
2022-03-31 00:14:08 +0200

    New User app & model

---
+++ backend/users/models.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError("Il manque l'adresse e-mail")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Un superuser doit avoir is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Un superuser doit avoir is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    username = None
+    email = models.EmailField(verbose_name="Adresse e-mail", unique=True)
+    first_name = models.CharField(verbose_name="Prénom", max_length=150)
+    last_name = models.CharField(verbose_name="Nom", max_length=150)
+
+    # is_active, is_staff, is_superuser
+    # date_joined, last_login
+
+    created = models.DateTimeField(verbose_name="Date de création", default=timezone.now)
+    updated = models.DateTimeField(verbose_name="Date de mise à jour", auto_now=True)
+
+    class Meta:
+        verbose_name = "Utilisateur"
+        verbose_name_plural = "Utilisateurs"
