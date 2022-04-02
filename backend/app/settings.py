3c583bd434743f24a63dd9beddcae6adf44a27b4
Raphael Odini <raphodn@users.noreply.github.com>
2022-04-02 14:09:52 +0200

    Add footer

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -99,6 +99,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                # custom
+                "core.utils.settings_context_processors.expose_settings",
             ],
         },
     },
---
+++ backend/core/utils/settings_context_processors.py
@@ -0,0 +1,15 @@
+from core.models import Configuration
+
+
+configuration = Configuration.get_solo()
+
+
+def expose_settings(request):
+    """
+    Put things into the context to make them available in templates.
+    https://docs.djangoproject.com/en/2.1/ref/templates/api/#using-requestcontext
+    """
+
+    return {
+        "CONFIGURATION": configuration,
+    }
