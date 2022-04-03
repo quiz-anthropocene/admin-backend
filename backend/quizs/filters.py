c5926fa4dd245c9cb0b39103ec900e4a63813d48
Raphael Odini <raphodn@users.noreply.github.com>
2022-04-03 22:56:47 +0200

    Add filters on Quiz table

---
+++ backend/quizs/filters.py
@@ -0,0 +1,16 @@
+import django_filters
+
+from core import constants
+from quizs.models import Quiz
+from tags.models import Tag
+
+
+class QuizFilter(django_filters.FilterSet):
+    tags = django_filters.ModelChoiceFilter(label="Tag", queryset=Tag.objects.all())  # ModelMultipleChoiceFilter
+    # language = django_filters.ChoiceFilter(label="Language", choices=constants.LANGUAGE_CHOICES)
+    publish = django_filters.ChoiceFilter(label="Publish", choices=constants.BOOLEAN_CHOICES)
+    has_audio = django_filters.ChoiceFilter(label="Has audio", choices=constants.BOOLEAN_CHOICES)
+
+    class Meta:
+        model = Quiz
+        fields = ["tags", "language", "publish", "has_audio"]
