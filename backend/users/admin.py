348b6c86eed212f77db04c839a7db90a78735b10
Raphael Odini <raphodn@users.noreply.github.com>
2022-04-04 02:31:55 +0200

    Backend: fix User admin display & create

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -12,7 +12,25 @@ class UserAdmin(UserAdmin):
         "email",
         "created",
     )
+    list_filter = ["is_staff"]
     ordering = ["-created"]
+
+    readonly_fields = ["is_active", "is_staff", "is_superuser", "last_login", "created", "updated"]
+    fieldsets = (
+        (None, {"fields": ("first_name", "last_name", "email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        ("Dates", {"fields": ("last_login", "created", "updated")}),
+    )
+    add_fieldsets = ((None, {"fields": ("first_name", "last_name", "email", "password1", "password2")}),)
 
 
 admin_site.register(User, UserAdmin)
