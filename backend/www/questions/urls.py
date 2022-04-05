cb38ac51643ae9fa280a2af7c588ffcef036479e
Raphael Odini <raphodn@users.noreply.github.com>
2022-04-06 01:23:26 +0200

    Basic question detail page

--- backend/www/questions/urls.py
+++ backend/www/questions/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from www.questions.views import QuestionListView
+from www.questions.views import QuestionDetailView, QuestionListView
 
 
 app_name = "questions"
 
 urlpatterns = [
     path("", QuestionListView.as_view(), name="list"),
+    path("<int:pk>/", QuestionDetailView.as_view(), name="detail"),
 ]
--- backend/www/questions/views.py
+++ backend/www/questions/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.forms.models import model_to_dict
+from django.views.generic import DetailView
 from django_filters.views import FilterView
 from django_tables2.views import SingleTableMixin
 
@@ -24,4 +26,16 @@ class QuestionListView(LoginRequiredMixin, SingleTableMixin, FilterView):
         request_params = [value for (key, value) in self.request.GET.items() if ((key not in ["page"]) and value)]
         if len(request_params):
             context["active_filters"] = True
+        return context
+
+
+class QuestionDetailView(LoginRequiredMixin, DetailView):
+    model = Question
+    template_name = "questions/detail.html"
+    context_object_name = "question"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["question_dict"] = model_to_dict(self.get_object())
+        # print(context["question_dict"])
         return context
