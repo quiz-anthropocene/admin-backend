3696969f5cc9c708f36eb1e2055fc471b4b7868b
Raphael Odini <raphodn@users.noreply.github.com>
2022-04-06 00:20:06 +0200

    Only show Contribution question id & quiz id

--- backend/contributions/tables.py
+++ backend/contributions/tables.py
@@ -1,16 +1,19 @@
 import django_tables2 as tables
 
 from contributions.models import Question
-from core.tables import ChoiceColumn, LongTextEllipsisColumn
+from core.tables import ChoiceColumn
 
 
 class ContributionTable(tables.Table):
     type = ChoiceColumn()
-    text = LongTextEllipsisColumn(attrs={"td": {"title": lambda record: record.text}})
-    # description
+    # text = LongTextEllipsisColumn(attrs={"td": {"title": lambda record: record.text}})
+    question = tables.Column(
+        verbose_name="Question", accessor="question.id", attrs={"td": {"title": lambda record: record.question}}
+    )
+    quiz = tables.Column(verbose_name="Quiz", accessor="quiz.id", attrs={"td": {"title": lambda record: record.quiz}})
 
     class Meta:
         model = Question
         template_name = "django_tables2/bootstrap4.html"
-        fields = ["type", "text", "question", "quiz", "created"]
+        fields = ["type", "text", "question", "quiz", "created"]  # description
         attrs = {"class": "table-responsive table-striped table-bordered border-primary font-size-small"}
