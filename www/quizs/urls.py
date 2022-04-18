e55d5fe9a5c15a6c2b795a593c933c3aa9fa31fb
Raphael Odini <raphodn@users.noreply.github.com>
2022-04-18 23:11:24 +0200

    Interface d'administration 6/X : lien entre questions & quizs (#1760)
    * Quiz: tab with list of questions
    
    * Quiz: show question_count & contribution_count in tabs
    
    * Question: tab with list of quizs

--- www/quizs/urls.py
+++ www/quizs/urls.py
@@ -1,7 +1,13 @@
 from django.urls import include, path
 from django.views.generic.base import RedirectView
 
-from www.quizs.views import QuizDetailContributionsView, QuizDetailStatsView, QuizDetailView, QuizListView
+from www.quizs.views import (
+    QuizDetailContributionsView,
+    QuizDetailQuestionsView,
+    QuizDetailStatsView,
+    QuizDetailView,
+    QuizListView,
+)
 
 
 app_name = "quizs"
@@ -20,6 +26,7 @@ urlpatterns = [
                     name="detail",
                 ),
                 path("view/", QuizDetailView.as_view(), name="detail_view"),
+                path("questions/", QuizDetailQuestionsView.as_view(), name="detail_questions"),
                 path("comments/", QuizDetailContributionsView.as_view(), name="detail_contributions"),
                 path("stats/", QuizDetailStatsView.as_view(), name="detail_stats"),
             ]
