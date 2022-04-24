e9f4eee65a261b1c4862c3990e13f6a0946df60b
Raphael Odini <raphodn@users.noreply.github.com>
2022-04-24 16:10:45 +0200

    Add tag filter by name

---
+++ backend/tags/filters.py
@@ -0,0 +1,21 @@
+import django_filters
+
+from tags.models import Tag
+
+
+class TagFilter(django_filters.FilterSet):
+    q = django_filters.CharFilter(
+        label="Nom du tag",
+        method="name_search",
+        required=True,
+    )
+
+    class Meta:
+        model = Tag
+        fields = ["q"]
+
+    def name_search(self, queryset, name, value):
+        if not value:
+            return queryset
+        # normal name filtering
+        return queryset.filter(name__icontains=value)
--- backend/www/tags/views.py
+++ backend/www/tags/views.py
@@ -3,26 +3,36 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, UpdateView
-from django_tables2.views import SingleTableView
+from django_filters.views import FilterView
+from django_tables2.views import SingleTableMixin, SingleTableView
 
 from api.tags.serializers import TagSerializer
 from questions.models import Question
 from quizs.models import Quiz
+from tags.filters import TagFilter
 from tags.forms import TagEditForm
 from tags.models import Tag
 from tags.tables import TagTable
 
 
-class TagListView(LoginRequiredMixin, SingleTableView):
+class TagListView(LoginRequiredMixin, SingleTableMixin, FilterView):
     model = Tag
     template_name = "tags/list.html"
     context_object_name = "tags"
     table_class = TagTable
+    filterset_class = TagFilter
 
     def get_queryset(self):
         qs = super().get_queryset()
         qs = qs.prefetch_related("questions", "quizs")
         return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        request_params = [value for (key, value) in self.request.GET.items() if ((key not in ["page"]) and value)]
+        if len(request_params):
+            context["active_filters"] = True
+        return context
 
 
 class TagDetailView(LoginRequiredMixin, DetailView):
