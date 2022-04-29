1628a2a5f926819a339efe9f38ac454800b2cf83
Raphael Odini <raphodn@users.noreply.github.com>
2022-04-29 21:18:10 +0200

    Reorganize profile

--- backend/www/profile/urls.py
+++ backend/www/profile/urls.py
@@ -1,17 +1,12 @@
 from django.urls import path
-from django.views.generic.base import RedirectView
 
-from www.profile.views import ProfileInfoView, ProfileQuestionsView, ProfileQuizsView
+from www.profile.views import ProfileHomeView, ProfileInfoView, ProfileQuestionsView, ProfileQuizsView
 
 
 app_name = "profile"
 
 urlpatterns = [
-    path(
-        "",
-        RedirectView.as_view(pattern_name="profile:info", permanent=False),
-        name="detail",
-    ),
+    path("", ProfileHomeView.as_view(), name="home"),
     path("info/", ProfileInfoView.as_view(), name="info"),
     path("questions/", ProfileQuestionsView.as_view(), name="questions"),
     path("quizs/", ProfileQuizsView.as_view(), name="quizs"),
