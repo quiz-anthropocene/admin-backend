9f54caee8332882ef43dde9d280e192466c97765
Raphael Odini <raphodn@users.noreply.github.com>
2022-04-29 20:34:25 +0200

    Display user roles in profile

---
+++ backend/core/templatetags/array_choices_display.py
@@ -0,0 +1,25 @@
+from django import template
+from django.utils.encoding import force_str
+
+from users.models import User
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def array_choices_display(obj, field):
+    """Pretty rendering of ArrayField with choices."""
+
+    choices_dict = dict()
+
+    if field == "roles":
+        choices_dict = dict(User.USER_ROLE_CHOICES)
+
+    try:
+        keys = obj.get(field, [])
+    except:  # noqa
+        keys = getattr(obj, field, [])
+
+    value_display_list = [force_str(choices_dict.get(key, "")) for key in (keys or [])]
+    return ", ".join(filter(None, value_display_list))
