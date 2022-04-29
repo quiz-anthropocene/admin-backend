1b8d01f5a824030ec4fb7dbf78c6c930ecb2e7a6
Raphael Odini <raphodn@users.noreply.github.com>
2022-04-29 18:34:29 +0200

    Add simple tests

---
+++ backend/www/questions/tests.py
@@ -0,0 +1,57 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from questions.factories import QuestionFactory
+from users.factories import DEFAULT_PASSWORD, UserFactory
+
+
+QUESTION_EDIT_URLS = [
+    "questions:detail_view",
+    "questions:detail_edit",
+    "questions:detail_quizs",
+    "questions:detail_contributions",
+    "questions:detail_stats",
+]
+
+
+class QuestionListView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory()
+        cls.question_1 = QuestionFactory()
+        cls.question_2 = QuestionFactory()
+
+    def test_anonymous_user_cannot_access_question_list(self):
+        url = reverse("questions:list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/accounts/login/?next=/questions/")
+
+    def test_user_can_access_question_list(self):
+        self.client.login(email=self.user.email, password=DEFAULT_PASSWORD)
+        url = reverse("questions:list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["questions"]), 2)
+
+
+class QuestionDetailView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory()
+        cls.question_1 = QuestionFactory()
+        cls.question_2 = QuestionFactory()
+
+    def test_anonymous_user_cannot_access_question_detail(self):
+        for edit_url in QUESTION_EDIT_URLS:
+            url = reverse(edit_url, args=[self.question_1.id])
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 302)
+            self.assertTrue(response.url.startswith("/accounts/login/"))
+
+    def test_user_can_access_question_detail(self):
+        self.client.login(email=self.user.email, password=DEFAULT_PASSWORD)
+        url = reverse("questions:detail_view", args=[self.question_1.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["question"].id, self.question_1.id)
