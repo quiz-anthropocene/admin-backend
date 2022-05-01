b2d77bf4f567d6fe967c276658f245b162728d19
Raphael Odini <raphodn@users.noreply.github.com>
2022-05-01 18:24:11 +0200

    Interface d'administration : créer un quiz (#1812)
    * Add simple Quiz create form
    
    * Add Quiz create button in profile
    
    * Add tests

--- backend/quizs/forms.py
+++ backend/quizs/forms.py
@@ -7,17 +7,31 @@ from tags.models import Tag
 
 
 QUIZ_FORM_FIELDS = [field_name for field_name in QUIZ_FIELD_SEQUENCE if field_name not in Quiz.QUIZ_READONLY_FIELDS]
+QUIZ_CREATE_FORM_FIELDS = [field_name for field_name in QUIZ_FORM_FIELDS if field_name not in ["publish", "spotlight"]]
 
 
-class QuizEditForm(forms.ModelForm):
+class QuizCreateForm(forms.ModelForm):
     class Meta:
         model = Quiz
-        fields = QUIZ_FORM_FIELDS
+        fields = QUIZ_CREATE_FORM_FIELDS
         widgets = {}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["tags"].queryset = Tag.objects.all().order_by("name")
+        # self.fields["publish"].disabled = True
+        # self.fields["spotlight"].disabled = True
+
+
+class QuizEditForm(QuizCreateForm):
+    class Meta:
+        model = Quiz
+        fields = QUIZ_FORM_FIELDS
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields["publish"].disabled = False
+    #     self.fields["spotlight"].disabled = False
 
 
 class QuizQuestionEditForm(forms.ModelForm):
