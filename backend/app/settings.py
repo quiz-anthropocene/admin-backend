3c08aff2d09d85650711890cf71d7142c0e10507
Raphael Odini <raphodn@users.noreply.github.com>
2022-05-01 23:33:27 +0200

    Update serializers

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -229,6 +229,7 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
+    "DATETIME_FORMAT": "%Y-%m-%d",
 }
 
 
