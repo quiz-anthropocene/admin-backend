c51220dfa88b1d10756c98da7e8d5906f6408660
Raphael Odini <raphodn@users.noreply.github.com>
2022-05-04 12:55:47 +0200

    Add 404 & 500 page

--- backend/www/pages/urls.py
+++ backend/www/pages/urls.py
@@ -9,4 +9,6 @@ app_name = "pages"
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("403/", TemplateView.as_view(template_name="403.html"), name="403"),
+    path("404/", TemplateView.as_view(template_name="404.html"), name="404"),
+    path("500/", TemplateView.as_view(template_name="500.html"), name="500"),
 ]
