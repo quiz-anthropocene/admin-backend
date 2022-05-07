06259d827e1570b894d27ba61dd8450afa615298
Raphael Odini <raphodn@users.noreply.github.com>
2022-05-07 13:41:49 +0200

    Add User admin role filter

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,28 @@
+from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from core.admin import admin_site
+from users import constants
 from users.models import User
+
+
+class RoleFilter(admin.SimpleListFilter):
+    title = User._meta.get_field("roles").verbose_name
+    parameter_name = "roles"
+
+    def lookups(self, request, model_admin):
+        return constants.USER_ROLE_CHOICES
+
+    def queryset(self, request, queryset):
+        lookup_value = self.value()
+        if lookup_value:
+            if lookup_value == constants.USER_ROLE_CONTRIBUTOR:
+                return queryset.all_contributors()
+            if lookup_value == constants.USER_ROLE_SUPER_CONTRIBUTOR:
+                return queryset.all_super_contributors()
+            if lookup_value == constants.USER_ROLE_ADMINISTRATOR:
+                return queryset.all_administrators()
+        return queryset
 
 
 class UserAdmin(UserAdmin):
@@ -12,7 +33,7 @@ class UserAdmin(UserAdmin):
         "email",
         "created",
     )
-    list_filter = ["is_staff"]
+    list_filter = [RoleFilter, "is_staff"]
     search_fields = ["id", "first_name", "last_name", "email"]
     ordering = ["-created"]
 
