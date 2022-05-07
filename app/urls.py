25f0db861fa8e7ac7c0a38c4ead154d79850b1be
Raphael Odini <raphodn@users.noreply.github.com>
2022-05-07 16:06:23 +0200

    Utiliser le CRON de Scalingo (#1838)
    * Remove github actions with Notion
    
    * Add cron.json
    
    * Cleanup app views & urls

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.urls import include, path
 
-from app import views
 from core.admin import admin_site
 
 
@@ -21,18 +20,6 @@ urlpatterns = [
     path("api/", include("api.urls")),
     # stats
     path("stats/", include("stats.urls")),
-    # actions
-    path("actions/aggregate-stats", views.action_aggregate_stats),
-    path(
-        "actions/import-questions-from-notion",
-        views.action_import_questions_from_notion,
-    ),
-    path("actions/export-data-to-github", views.action_export_data_to_github),
-    path("actions/export-stats-to-github", views.action_export_stats_to_github),
-    path(
-        "actions/export-contributions-to-notion",
-        views.action_export_contributions_to_notion,
-    ),
 ]
 
 if settings.DEBUG:  # and "debug_toolbar" in settings.INSTALLED_APPS:
