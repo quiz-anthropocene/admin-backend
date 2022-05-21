8c2d476abc05126c76ffe7f51c1131036d826528
dq18 <didierquirin@gmail.com>
2022-05-21 19:40:36 +0200

    added filter by id for quizs and questions

--- questions/filters.py
+++ questions/filters.py
@@ -10,6 +10,7 @@ class QuestionFilter(django_filters.FilterSet):
     # )  # empty_label="--- Type ---"
     # category = django_filters.ModelChoiceFilter(label="Category", queryset=Category.objects.all())
     tags = django_filters.ModelMultipleChoiceFilter(label="Tag", queryset=Tag.objects.all())
+    id = django_filters.NumberFilter(label="Id")
     # difficulty = django_filters.ChoiceFilter(label="Difficulty", choices=constants.QUESTION_DIFFICULTY_CHOICES)
     # language = django_filters.ChoiceFilter(label="Language", choices=constants.LANGUAGE_CHOICES)
     # validation_status = django_filters.ChoiceFilter(
@@ -18,4 +19,14 @@ class QuestionFilter(django_filters.FilterSet):
 
     class Meta:
         model = Question
-        fields = ["type", "category", "tags", "difficulty", "language", "author", "validation_status", "visibility"]
+        fields = [
+            "type",
+            "category",
+            "tags",
+            "difficulty",
+            "language",
+            "author",
+            "validation_status",
+            "visibility",
+            "id",
+        ]
--- quizs/filters.py
+++ quizs/filters.py
@@ -6,6 +6,7 @@ from tags.models import Tag
 
 
 class QuizFilter(django_filters.FilterSet):
+    id = django_filters.NumberFilter(label="Id")
     tags = django_filters.ModelChoiceFilter(label="Tag", queryset=Tag.objects.all())  # ModelMultipleChoiceFilter
     # language = django_filters.ChoiceFilter(label="Language", choices=constants.LANGUAGE_CHOICES)
     publish = django_filters.ChoiceFilter(label="Publish", choices=constants.BOOLEAN_CHOICES)
@@ -13,4 +14,4 @@ class QuizFilter(django_filters.FilterSet):
 
     class Meta:
         model = Quiz
-        fields = ["tags", "language", "publish", "has_audio", "author", "visibility"]
+        fields = ["tags", "language", "publish", "has_audio", "author", "visibility", "id"]
