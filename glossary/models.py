15317415bcb67d0c978a94b621df8cf191c8cd10
Raphael Odini <raphodn@users.noreply.github.com>
2022-05-21 13:52:57 +0200

    Cleanup du modèle

--- glossary/models.py
+++ glossary/models.py
@@ -8,16 +8,12 @@ class GlossaryItem(models.Model):
     description = models.TextField(verbose_name="Description", blank=True)
     description_accessible_url = models.URLField(verbose_name="Lien pour aller plus loin", max_length=500, blank=True)
     # timestamps
-    added = models.DateField(blank=True, null=True, help_text="La date d'ajout du mot")
     created = models.DateTimeField(verbose_name="Date de création", auto_now_add=True)
     updated = models.DateTimeField(verbose_name="Date de dernière modification", auto_now=True)
 
     class Meta:
         ordering = ["name"]
-
-    #     constraints = [
-    #         models.UniqueConstraint(fields=["name"], name="glossary_name_unique")
-    #     ]
+        constraints = [models.UniqueConstraint(fields=["name"], name="glossary_name_unique")]
 
     def __str__(self):
         return f"{self.name}"
