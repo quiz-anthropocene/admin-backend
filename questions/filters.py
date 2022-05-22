a22c8b48a62b748a4f46afd1a9949747a7aad52f
dq18 <didierquirin@gmail.com>
2022-05-22 14:39:42 +0200

    Filtre par ID sur les tables Question & Quiz (#1876)
    * added filter by id for quizs and questions
    
    * Add min value. Cleanup labels. Add multiselect on quiz.tags
    
    Co-authored-by: Raphael Odini <raphodn@users.noreply.github.com>

--- questions/filters.py
+++ questions/filters.py
@@ -1,21 +1,24 @@
 import django_filters
+from django.forms import NumberInput
 
 from questions.models import Question
 from tags.models import Tag
 
 
 class QuestionFilter(django_filters.FilterSet):
-    # type = django_filters.ChoiceFilter(
-    #     label="Type", choices=constants.QUESTION_TYPE_CHOICES
-    # )  # empty_label="--- Type ---"
-    # category = django_filters.ModelChoiceFilter(label="Category", queryset=Category.objects.all())
-    tags = django_filters.ModelMultipleChoiceFilter(label="Tag", queryset=Tag.objects.all())
-    # difficulty = django_filters.ChoiceFilter(label="Difficulty", choices=constants.QUESTION_DIFFICULTY_CHOICES)
-    # language = django_filters.ChoiceFilter(label="Language", choices=constants.LANGUAGE_CHOICES)
-    # validation_status = django_filters.ChoiceFilter(
-    #     label="Validation status", choices=constants.QUESTION_VALIDATION_STATUS_CHOICES
-    # )
+    id = django_filters.NumberFilter(widget=NumberInput(attrs={"min": 0}))
+    tags = django_filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all())
 
     class Meta:
         model = Question
-        fields = ["type", "category", "tags", "difficulty", "language", "author", "validation_status", "visibility"]
+        fields = [
+            "id",
+            "type",
+            "category",
+            "tags",
+            "difficulty",
+            "language",
+            "author",
+            "validation_status",
+            "visibility",
+        ]
