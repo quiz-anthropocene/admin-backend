059f551e57300eac39e5f508f9d50d6968791cec
Raphael Odini <raphodn@users.noreply.github.com>
2022-05-27 01:26:49 +0200

    Sendinblue method to add user to list

--- core/utils/sendinblue.py
+++ core/utils/sendinblue.py
@@ -6,17 +6,32 @@ from django.conf import settings
 from core.models import Configuration
 
 
-def newsletter_registration(email):
+HEADERS = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "api-key": settings.SIB_API_KEY,
+}
+
+
+def add_to_contact_list(user_email, list_id=settings.SIB_CONTRIBUTOR_LIST_ID, attributes=dict()):
+    data = {
+        "email": user_email,
+        "listIds": [int(list_id)],
+        "attributes": attributes,  # FIRSTNAME, LASTNAME
+        "updateEnabled": True,
+    }
+    return requests.post(settings.SIB_CONTACT_ENDPOINT, headers=HEADERS, data=json.dumps(data))
+
+
+def newsletter_registration(user_email):
+    """
+    Use Double-Opt-In Flow
+    https://developers.sendinblue.com/reference/createdoicontact
+    """
     configuration = Configuration.get_solo()
 
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "api-key": settings.SIB_API_KEY,
-    }
-
     data = {
-        "email": email,
+        "email": user_email,
         # "attributes": {
         #     "FIRSTNAME": "",
         #     "LASTNAME": ""
@@ -27,4 +42,4 @@ def newsletter_registration(email):
         "updateEnabled": True,
     }
 
-    return requests.post(settings.SIB_CONTACT_DOI_ENDPOINT, headers=headers, data=json.dumps(data))
+    return requests.post(settings.SIB_CONTACT_DOI_ENDPOINT, headers=HEADERS, data=json.dumps(data))
