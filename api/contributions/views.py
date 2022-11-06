209029a1d3c04815dec0a283252b939613c0a4ca
Raphael Odini <raphodn@users.noreply.github.com>
2022-11-06 19:25:58 +0100

    Enlever l'ancien package notion-py (#1917)
    * Remove reference to unofficial Notion API
    
    * Remove notion package
    
    * Remove extra reference
    
    * Update messages in Django admin

--- api/contributions/views.py
+++ api/contributions/views.py
@@ -11,6 +11,4 @@ class ContributionViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
 
     @extend_schema(summary="Nouvelle contribution", tags=[Contribution._meta.verbose_name], exclude=True)
     def create(self, request, *args, **kwargs):
-        # send to Notion ?
-        # done with export_contributions_to_notion + cron_export_contributions_to_notion
         return super().create(request, args, kwargs)
--- core/utils/notion.py
+++ core/utils/notion.py
@@ -1,11 +1,9 @@
 import json
 from datetime import date, datetime
 
-import notion  # https://github.com/jamalex/notion-py
 import requests
 from django.conf import settings
 from django.db.models.fields import URLField
-from notion.client import NotionClient
 
 from questions.models import Question
 
@@ -122,120 +120,3 @@ def process_page_properties(page_properties):
             )  # noqa
 
     return page_dict
-
-
-# Unofficial API (notion-py)
-
-
-def get_notion_client():
-    """
-    Connection
-    """
-    # try:
-    #     client = NotionClient(token_v2=settings.NOTION_TOKEN_V2)
-    # except Exception as e:
-    #     # usually: 401 Client Error: Unauthorized for url: https://www.notion.so/api/v3/loadUserContent # noqa
-    #     print(e)
-    #     raise
-    client = NotionClient(token_v2=settings.NOTION_TOKEN_V2)
-    return client
-
-
-def get_questions_table():
-    """
-    Questions page: get table
-    """
-    notion_client = get_notion_client()
-    # page = client.get_block(settings.NOTION_QUESTIONS_PAGE_URL)
-    table = notion_client.get_collection_view(settings.NOTION_QUESTIONS_TABLE_URL)
-    return table
-
-
-def get_questions_table_rows():
-    """
-    Questions page: get current rows
-    """
-    questions_table = get_questions_table()
-    rows = questions_table.collection.get_rows()
-    return rows
-
-
-def get_contribution_table():
-    """
-    Contribution page: get table
-    """
-    notion_client = get_notion_client()
-    # page = client.get_block(settings.NOTION_CONTRIBUTION_PAGE_URL)
-    table = notion_client.get_collection_view(settings.NOTION_CONTRIBUTION_TABLE_URL)
-    return table
-
-
-def add_contribution_row(
-    contribution_text: str,
-    contribution_description: str,
-    contribution_type: str,
-    contribution_date=datetime.now(),
-):
-    """
-    Contribution page: add row
-    """
-    # init
-    contribution_table = get_contribution_table()
-    # add row
-    row = contribution_table.collection.add_row()
-    row.type = contribution_type
-    row.text = contribution_text
-    row.description = contribution_description
-    # row.created = notion.collection.NotionDate(date.today())
-    row.created = notion.collection.NotionDate(contribution_date)
-    return row
-
-
-def get_import_stats_table():
-    """
-    Import stats page: get table
-    """
-    notion_client = get_notion_client()
-    # page = client.get_block(settings.NOTION_IMPORT_STATS_PAGE_URL)
-    table = notion_client.get_collection_view(settings.NOTION_IMPORT_STATS_TABLE_URL + "coucou")
-    return table
-
-
-def add_import_stats_row(total, new, update):
-    """
-    Import stats page: add row
-    """
-    # init
-    import_stats_table = get_import_stats_table()
-    # add row
-    row = import_stats_table.collection.add_row()
-    row.action = "import"
-    row.question_total = total
-    row.question_new = new
-    row.question_update = update
-    row.date = notion.collection.NotionDate(datetime.now())
-    return row
-
-
-def get_glossary_table():
-    """
-    Glossary page: get table
-    """
-    notion_client = get_notion_client()
-    # page = client.get_block(settings.NOTION_GLOSSARY_PAGE_URL)
-    table = notion_client.get_collection_view(settings.NOTION_GLOSSARY_TABLE_URL)
-    return table
-
-
-def get_glossary_rows():
-    """
-    Glossary page: get current rows
-    """
-    glossary_table = get_glossary_table()
-    # sort_params = [{
-    #     "property": "name",
-    #     "direction": "descending",
-    # }]
-    # glossary_table_sorted = glossary_table.build_query(sort=sort_params).execute()
-    rows = glossary_table.collection.get_rows()
-    return rows
