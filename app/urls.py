c06f639b1e0d77d8be3c587c15f5db40f6018689
Raphael Odini <raphodn@users.noreply.github.com>
2023-02-14 14:54:29 +0100

    Only prefix 'www' urls

--- app/urls.py
+++ app/urls.py
@@ -5,8 +5,7 @@ from django.urls import include, path
 from core.admin import admin_site
 
 
-urlpatterns = i18n_patterns(
-    # www
+contribution_patterns = i18n_patterns(
     path("", include("www.pages.urls")),
     path("accounts/", include("www.auth.urls")),
     path("profile/", include("www.profile.urls")),
@@ -17,14 +16,18 @@ urlpatterns = i18n_patterns(
     path("contributions/", include("www.contributions.urls")),
     path("glossary/", include("www.glossary.urls")),
     path("admin/", include("www.admin.urls")),
+)
+
+urlpatterns = contribution_patterns + [
+    # set_language
+    path("i18n/", include("django.conf.urls.i18n")),
     # admin
     path("django/", admin_site.urls),
     # api
     path("api/", include("api.urls")),
     # stats
     path("stats/", include("stats.urls")),
-    path("i18n/", include("django.conf.urls.i18n")),
-)
+]
 
 if settings.DEBUG:  # and "debug_toolbar" in settings.INSTALLED_APPS:
     urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
