4151a351b0ad2f4710ce64cff35306dd052552e2
Raphael Odini <raphodn@users.noreply.github.com>
2023-02-23 14:51:53 +0100

    Traduction du modèle Glossaire (#1948)
    * Translate Glossary model
    
    * Add translations

--- glossary/models.py
+++ glossary/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from simple_history.models import HistoricalRecords
 
 from history.models import HistoryChangedFieldsAbstractModel
@@ -10,14 +11,16 @@ class GlossaryItem(models.Model):
     GLOSSARY_URL_FIELDS = ["description_accessible_url"]
     GLOSSARY_ITEM_READONLY_FIELDS = ["created", "updated"]
 
-    name = models.CharField(verbose_name="Mot ou sigle", max_length=50, blank=False)
-    name_alternatives = models.TextField(verbose_name="Noms alternatifs", blank=True)  # ArrayField
-    definition_short = models.CharField(verbose_name="Définition (succinte)", max_length=150, blank=False)
-    description = models.TextField(verbose_name="Description", blank=True)
-    description_accessible_url = models.URLField(verbose_name="Lien pour aller plus loin", max_length=500, blank=True)
+    name = models.CharField(verbose_name=_("Word or acronym"), max_length=50, blank=False)
+    name_alternatives = models.TextField(verbose_name=_("Alternative names"), blank=True)  # ArrayField
+    definition_short = models.CharField(verbose_name=_("Définition (short)"), max_length=150, blank=False)
+    description = models.TextField(verbose_name=_("Description"), blank=True)
+    description_accessible_url = models.URLField(
+        verbose_name=_("Accessible source (link)"), max_length=500, blank=True
+    )
 
-    created = models.DateTimeField(verbose_name="Date de création", default=timezone.now)
-    updated = models.DateTimeField(verbose_name="Date de dernière modification", auto_now=True)
+    created = models.DateTimeField(verbose_name=_("Creation date"), default=timezone.now)
+    updated = models.DateTimeField(verbose_name=_("Last update date"), auto_now=True)
 
     history = HistoricalRecords(bases=[HistoryChangedFieldsAbstractModel])
 
