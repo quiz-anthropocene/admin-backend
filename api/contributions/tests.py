57f6890e22ede4ca27c119ea0a70b47bd5e8a759
Raphael Odini <raphodn@users.noreply.github.com>
2023-03-23 23:15:31 +0100

    Remove contribution-list API endpoint

--- api/contributions/tests.py
+++ api/contributions/tests.py
@@ -1,26 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from contributions.models import Comment
 from core import constants
 from questions.factories import QuestionFactory
-
-
-class CommentApiTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.question = QuestionFactory()
-        cls.comment_app = Comment.objects.create(type=constants.COMMENT_TYPE_COMMENT_APP)
-        cls.comment_question_published = Comment.objects.create(
-            type=constants.COMMENT_TYPE_COMMENT_QUESTION, question=cls.question, publish=True
-        )
-
-    def test_comment_list(self):
-        url = reverse("api:contribution-list")
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(response.data["results"], list)
-        self.assertEqual(len(response.data["results"]), 1)
 
 
 class CommentCreateApiTest(TestCase):
