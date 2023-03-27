00ddba613ec8813dcc2f5c2b69a5df2550161273
dq18 <didierquirin@gmail.com>
2023-03-28 00:03:48 +0200

    reorder language middlewares

--- app/settings.py
+++ app/settings.py
@@ -95,14 +95,16 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",  # whitenoise
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "know-your-planet.middlewares.LanguageMiddleware",
     "corsheaders.middleware.CorsMiddleware",  # django-cors-headers
     "simple_history.middleware.HistoryRequestMiddleware",  # django-simple-history
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.locale.LocaleMiddleware",
 ]
 
 ROOT_URLCONF = "app.urls"
