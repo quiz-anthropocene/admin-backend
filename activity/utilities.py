eecdf017c77271578782f072029b3eb45fa2215b
Raphael Odini <raphodn@users.noreply.github.com>
2023-04-11 00:07:46 +0200

    Refactor create_event

--- activity/utilities.py
+++ activity/utilities.py
@@ -1,35 +1,41 @@
 from django.utils import timezone
 
 from activity.models import Event
+from core.utils.utilities import truncate_with_ellipsis
 from users.models import User
 
 
-def create_event(user: User, event_verb: str, event_object, created=timezone.now()):
-    # actor_name
-    actor_name = user.full_name
-    if len(actor_name) > 150:
-        actor_name = actor_name[:147] + "..."
+def get_event_object_name(event_object, event_object_type):
+    event_object_name = ""
 
-    # event_object_type
-    event_object_type = event_object._meta.model._meta.model_name.upper()
-
-    # event_object_name
     if event_object_type in ["QUESTION"]:
         event_object_name = event_object.text
     elif event_object_type in ["USER"]:
         event_object_name = event_object.full_name
     else:  # "QUIZ"
         event_object_name = event_object.name
-    if len(event_object_name) > 150:
-        event_object_name = event_object_name[:147] + "..."
+
+    return truncate_with_ellipsis(event_object_name, 150)
+
+
+def create_event(user: User, event_verb: str, event_object, created=timezone.now()):
+    # init
+    event_dict = {"created": created}
+
+    # actor: id & name
+    if user:
+        event_dict["actor_id"] = user.id
+        event_dict["actor_name"] = truncate_with_ellipsis(user.full_name, 150)
+
+    # event_verb
+    if event_verb:
+        event_dict["event_verb"] = event_verb
+
+    # event_object: id, type, name
+    if event_object:
+        event_dict["event_object_id"] = event_object.id
+        event_dict["event_object_type"] = event_object._meta.model._meta.model_name.upper()
+        event_dict["event_object_name"] = get_event_object_name(event_object, event_dict["event_object_type"])
 
     # create event
-    Event.objects.create(
-        actor_id=user.id,
-        actor_name=actor_name,
-        event_verb=event_verb,
-        event_object_id=event_object.id,
-        event_object_type=event_object_type,
-        event_object_name=event_object_name,
-        created=created,
-    )
+    Event.objects.create(**event_dict)
