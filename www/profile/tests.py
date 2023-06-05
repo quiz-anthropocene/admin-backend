57cbfc7b2fb39d0ce1de1c5607468460c7b798f6
Raphael Odini <raphodn@users.noreply.github.com>
2023-06-06 01:35:00 +0200

    Add tests

--- www/profile/tests.py
+++ www/profile/tests.py
@@ -5,6 +5,17 @@ from users import constants
 from users.factories import DEFAULT_PASSWORD, UserFactory
 
 
+PROFILE_DETAIL_URLS = [
+    # "profile:home",
+    "profile:info",
+    "profile:questions_view",
+    "profile:questions_stats",
+    "profile:quizs_view",
+    "profile:quizs_stats",
+    "profile:history",
+]
+
+
 class ProfileViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -27,3 +38,24 @@ class ProfileViewTest(TestCase):
         self.client.login(email=self.user_contributor.email, password=DEFAULT_PASSWORD)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
+
+
+class ProfileDetailViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory()
+
+    def test_anonymous_user_cannot_access_profile_detail(self):
+        for detail_url in PROFILE_DETAIL_URLS:
+            url = reverse(detail_url)
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 302)
+            self.assertIn("/accounts/login/?next=", response.url)
+
+    def test_contributor_can_access_profile_detail(self):
+        self.client.login(email=self.user.email, password=DEFAULT_PASSWORD)
+        for detail_url in PROFILE_DETAIL_URLS:
+            url = reverse(detail_url)
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.context["user"].id, self.user.id)
