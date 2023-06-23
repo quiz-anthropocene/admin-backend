b07f519cba9ee97c5ab3400043a109cc46b48866
Raphael Odini <raphodn@users.noreply.github.com>
2023-06-23 20:36:56 +0200

    Sendinblue: method to send transactional email with html

--- core/utils/sendinblue.py
+++ core/utils/sendinblue.py
@@ -63,17 +63,42 @@ def newsletter_registration(user_email):
 
 
 def send_transactional_email_with_template_id(
-    template_id,
     to_email,
     to_name,
+    template_id,
     parameters=None,
     from_email=settings.DEFAULT_FROM_EMAIL,
     from_name=settings.DEFAULT_FROM_NAME,
 ):
     data = {
-        "sender": {"email": from_email, "name": from_name},  # must be a sender registered and verified in Brevo
+        "sender": {"email": from_email, "name": from_name},  # email must be a sender registered and verified in Brevo
         "to": [{"email": to_email, "name": to_name}],
         "templateId": template_id,
+    }
+    if parameters:
+        data["params"] = parameters
+
+    if not settings.DEBUG and not settings.TESTING:
+        return requests.post(settings.SIB_SMTP_ENDPOINT, headers=HEADERS, data=json.dumps(data))
+    else:
+        print("Sendinblue: email not sent (DEBUT or TESTING environment detected)")
+        return True
+
+
+def send_transactional_email_with_html(
+    to_email,
+    to_name,
+    subject,
+    html,
+    parameters=None,
+    from_email=settings.DEFAULT_FROM_EMAIL,
+    from_name=settings.DEFAULT_FROM_NAME,
+):
+    data = {
+        "sender": {"email": from_email, "name": from_name},  # email must be a sender registered and verified in Brevo
+        "to": [{"email": to_email, "name": to_name}],
+        "subject": subject,
+        "htmlContent": html,
     }
     if parameters:
         data["params"] = parameters
